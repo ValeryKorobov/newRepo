7c573e241362999d64a94ee1b614bf8734511f2f
Valery Korobov <123397910+ValeryKorobov@users.noreply.github.com>
2023-08-07 19:57:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,15 @@ import os
 def get_count(num: int, sec_num: int) -> int:
     return num + sec_num
 
+def exist_count(res: int) -> bool:
+    return isinstance(res, int)
+
 
 def main():
     print("Введите по очереди два числа: ")
-    print(get_count(int(input()), int(input())))
+    res = get_count(int(input()), int(input()))
+    print(res)
+    print(exist_count(res))
 
 
 if __name__ == '__main__':
